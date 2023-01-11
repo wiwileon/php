10e10d24f449c12cdb5716474573e8577d674a84
wiwileon <wiwixxxl@gmail.com>
2023-01-12 00:27:38 +0300

    init

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -3,6 +3,36 @@ let operation = null;
 
 const inputWindow = document.getElementById('inputWindow');
 
+document.getElementById('btn_1').addEventListener('click', function () {
+   inputWindow.value += '1';
+})
+
+document.getElementById('btn_sum').addEventListener('click', function () {
+   lastOperand = parseInt(inputWindow.value);
+   operation = 'sum';
+   inputWindow.value = '';
+})
+
+document.getElementById('btn_def').addEventListener('click', function () {
+   lastOperand = parseInt(inputWindow.value);
+   operation = 'def';
+   inputWindow.value = '';
+})
+
+document.getElementById('btn_calc').addEventListener('click', function () {
+   if (operation === 'sum') {
+      const result = lastOperand + parseInt(inputWindow.value);
+      lastOperand = 0;
+      operation = null;
+      inputWindow.value = result;
+   }
+   if (operation === 'def') {
+      const result = lastOperand - parseInt(inputWindow.value);
+      lastOperand = 0;
+      operation = null;
+      inputWindow.value = result;
+   }
+})
 
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
@@ -10,3 +40,6 @@ document.getElementById('btn_clr').addEventListener('click', function () {
     inputWindow.value = '';
 })
 
+
+console.log(lastOperand);
+console.log(operation);
